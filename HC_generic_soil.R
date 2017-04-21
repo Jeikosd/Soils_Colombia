578df9b96451180c7269c8cc7a092e6ceb4ccf2a
Mesa Diez <j.mesa@cgiar.org>
2017-04-21 15:03:16 -0500

    add function that load position of HC soils

--- HC_generic_soil.R
+++ HC_generic_soil.R
@@ -1,4 +1,79 @@
 
+
+# function to calculte star and end to load line from HC.SOL
+# path_soil
+pos_load_HC <- function(path){
+  
+  # path <- path_soil
+  HC_soil <- read_lines(path)
+  
+  generic_soil <- str_subset(HC_soil, 'HC_GEN') %>%
+    sub(" .*$", "", .) %>%
+    str_sub(2)
+  
+  pos_extract <- str_which(HC_soil, "SLB")
+  # HC_soil[pos_extract]
+  init_extract <- pos_extract[seq(1,length(pos_extract),2)]
+  end_extract <- pos_extract[seq(2,length(pos_extract),2)] 
+  end_extract <- (end_extract - init_extract) - 1
+  
+  pos <- data_frame(generic_soil, init_extract, end_extract)
+  return(pos)
+}
+
+
+# function to load HC.SOL (DSSAT) (load and specifi soil)
+# path :  dir when the HC.SOL is.
+# start : line to start to load
+# end : line to end to load
+# you need to calcule start and end previously
+
+fread_HC<- function(path, start, end){
+  
+  # start <- init_extract[1]
+  # end <- end_extract[1]
+  # data <- paste0(path_data, 'Soil_HC/HC.SOL')
+  # fread(paste0(path_data, 'Soil_HC/HC.SOL'), skip = init_extract[1], header = F, fill = TRUE, nrows = end_extract[1]) %>%
+  # tbl_df()
+  require(tidyverse)
+  require(magrittr)
+  # positions
+  header <- scan(path, what = "character", skip = 5, nlines = 1, quiet = T)
+  header <- header[!str_detect('@', header)]
+  
+  start <- dplyr::select(positions, 1) %>%
+    magrittr::
+  
+  soil_df <- fread(path, skip = start, header = F, fill = TRUE, nrows = end) %>%
+    tbl_df() %>%
+    purrr::discard(is_logical)
+  
+  colnames(soil_df) <- header 
+  
+  return(soil_df)
+  
+  
+}
+
+
+##
+
+
+readHC_soil <- function(path, start, end) {
+  
+  ## path <- path_soil
+
+  
+  fread_HC_soil(path, start, end)
+  pmap(list(data, start, end), fread_HC_soil)
+  
+  data_frame(HC = generic_soil, start, end) %>%
+    mutate(inf_soils = pmap(list(data, init_extract, end_extract), fread_HC_soil))
+  
+} 
+
+
+
 calc_texture <- function(clay, silt, sand){
   
   require(soiltexture)
@@ -14,48 +89,3 @@ calc_texture <- function(clay, silt, sand){
   return(tbl_df(class_texture))
   
 }
-
-
-readHC_soil <- function(df) {
-  
-  
-  HC_soil <- read_lines(paste0(path_data, 'Soil_HC/HC.SOL'))
-  
-  generic_soil <- str_subset(HC_soil, 'HC_GEN') %>%
-    sub(" .*$", "", .) %>%
-    str_sub(2)
-  
-  pos_extract <- str_which(HC_soil, "SLB")
-  # HC_soil[pos_extract]
-  init_extract <- pos_extract[seq(1,length(pos_extract),2)]
-  end_extract <- pos_extract[seq(2,length(pos_extract),2)] 
-  end_extract <- (end_extract - init_extract) - 1
-  
-  fread_HC_soil <- function(data, start, end){
-    
-    # start <- init_extract[1]
-    # end <- end_extract[1]
-    # data <- paste0(path_data, 'Soil_HC/HC.SOL')
-    # fread(paste0(path_data, 'Soil_HC/HC.SOL'), skip = init_extract[1], header = F, fill = TRUE, nrows = end_extract[1]) %>%
-    # tbl_df()
-    
-    header <- scan(data, what = "character", skip = 5, nlines = 1, quiet = T)
-    header <- header[!str_detect('@', header)]
-    
-    soil_df <- fread(data, skip = start, header = F, fill = TRUE, nrows = end) %>%
-      tbl_df() %>%
-      purrr::discard(is_logical)
-    
-    colnames(soil_df) <- header 
-    
-    return(soil_df)
-    
-    
-  }
-  fread_HC_soil(paste0(path_data, 'Soil_HC/HC.SOL'), init_extract[1], end_extract[1])
-  pmap(list(data, init_extract, end_extract), fread_HC_soil)
-  
-  data_frame(HC = generic_soil, init_extract, end_extract) %>%
-    mutate(inf_soils = pmap(list(data, init_extract, end_extract), fread_HC_soil))
-  
-} 
